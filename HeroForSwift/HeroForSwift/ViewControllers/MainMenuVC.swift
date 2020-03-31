d6eebf412fc0490f638fadbbe4b44a9d39e3293c
Taeyoun Lee <sino.lty@gmail.com>
2020-04-01 06:17:14 +0900

    First Commit

---
+++ HeroForSwift/HeroForSwift/ViewControllers/MainMenuVC.swift
@@ -0,0 +1,31 @@
+//
+//  MainMenuVC.swift
+//  HeroForSwift
+//
+//  Created by Taeyoun Lee on 2020/04/01.
+//  Copyright © 2020 Sino. All rights reserved.
+//
+
+import UIKit
+import Hero
+
+class MainMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HeroForSwift/Pods/Target Support Files/Pods-HeroForSwift/Pods-HeroForSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeroForSwift : NSObject
+@end
+@implementation PodsDummy_Pods_HeroForSwift
+@end
