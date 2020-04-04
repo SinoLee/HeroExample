7bf9bced498b624aa3a05e4b816d23f7c9884b82
Taeyoun Lee <sino.lty@gmail.com>
2020-04-04 10:52:14 +0900

    Hero Example for Swift

--- HeroForSwift/HeroForSwift/ViewControllers/MainMenuVC.swift
+++ HeroForSwift/HeroForSwift/ViewControllers/MainMenuVC.swift
@@ -2,30 +2,15 @@
 //  MainMenuVC.swift
 //  HeroForSwift
 //
-//  Created by Taeyoun Lee on 2020/04/01.
+//  Created by Taeyoun Lee on 2020/04/04.
 //  Copyright © 2020 Sino. All rights reserved.
 //
 
 import UIKit
-import Hero
 
-class MainMenuVC: UIViewController {
+class MainMenuVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
